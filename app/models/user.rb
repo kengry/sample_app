bb5a870a99ac88f684a7900fea9c25ce80c2f26d
Nikita Demidov <kengry@gmail.com>
2013-08-12 18:59:08 +0400

    Add index to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,9 @@
 #
 
 class User < ActiveRecord::Base
+  # Ensuring email uniqueness by downcasing the email attribute. 
+  before_save { |user| user.email = email.downcase }
+  
   validates :name, presence: true, length: { maximum: 50 }
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
